cd419c2f5a86a8c9ae68d1a3eaf42ed7ccce3c26
tudorgrig <tudorgrigoriubb@gmail.com>
2016-01-07 13:27:15 +0200

    home page and expenses layout page added

---
+++ myMoneyTracker/www/app/app.js
@@ -0,0 +1,32 @@
+angular.module('myMoneyTracker', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider
+
+		.state('home',{
+			url : "/home",
+			templateUrl: "app/home/home.html"
+		})
+
+		.state('expenses', {
+			url : "/expenses",
+			templateUrl: "app/layout/expense-layout.html"
+		});
+	$urlRouterProvider.otherwise('/expenses');
+});
