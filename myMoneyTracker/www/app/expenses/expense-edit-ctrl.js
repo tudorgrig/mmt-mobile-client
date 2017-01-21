21263849cdd32480893f2afbeaa404b96ecc8f73
Florin Iacob <florin.e.iacob@gmail.com>
2017-01-22 00:05:09 +0200

    Fix EDIT Income/Expense/Category

--- myMoneyTracker/www/app/expenses/expense-edit-ctrl.js
+++ myMoneyTracker/www/app/expenses/expense-edit-ctrl.js
@@ -9,6 +9,23 @@
 		vm.categories = [];
 		categoryApi.getCategories(function (data) {
 			vm.categories = data;
+			vm.categorySelected = JSON.parse($stateParams['category']);
+			vm.expense = {
+      			id : $stateParams['id'],
+      			name : $stateParams['name'],
+      			description : $stateParams['description'],
+      			category : null,
+      			amount : parseFloat($stateParams['amount']),
+      			creationDate : new Date(parseInt($stateParams['creationDate'])),
+      			currency : $stateParams['currency'],
+      			frequency : $stateParams['frequency']
+      		};
+			var i;
+			for (i = 0; i <vm.categories.length; i++) {
+				if (vm.categorySelected.name === vm.categories[i].name) {
+					vm.expense.category = vm.categories[i];
+				}
+			}
 		})
 		vm.disableNoInternet;
     if($window.localStorage['hasInternet'] != undefined) {
@@ -19,17 +36,7 @@
                   vm.disableNoInternet = !JSON.parse($window.localStorage['hasInternet']);
            }
         }, 1000)
-		vm.expense = {
-			id : $stateParams['id'],
-			name : $stateParams['name'],
-			description : $stateParams['description'],
-			category : JSON.parse($stateParams['category']),
-			amount : parseFloat($stateParams['amount']),
-			creationDate : new Date(parseInt($stateParams['creationDate'])),
-			currency : $stateParams['currency'],
-			frequency : parseInt($stateParams['frequency'])
 
-		};
     console.log(vm.expense);
 		vm.updateExpense = function () {
 			expenseApi.updateExpense(vm.expense, $stateParams['index']);
